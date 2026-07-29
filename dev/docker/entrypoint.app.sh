c2715358ed40d0673537e6c7a31a4668a7a5bd0a
Jônathas Assunção <jonathas.assuncao@cpb.com.br>
2026-07-29 20:47:10 -0300

    feat: Ajustes para desenvolvimento

--- dev/docker/entrypoint.app.sh
+++ dev/docker/entrypoint.app.sh
@@ -8,8 +8,8 @@ if [[ -n "$1" ]]; then
     exec "$@"
 else
     composer install
-    wait-for-it db:3306 -t 45
-    php artisan migrate --database=mysql --force
+    # wait-for-it db:3306 -t 45
+    # php artisan migrate --database=mysql --force
     chown -R www-data storage public/uploads bootstrap/cache
     exec apache2-foreground
 fi
